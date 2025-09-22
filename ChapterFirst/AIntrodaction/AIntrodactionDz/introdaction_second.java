8dc53b8608341d950522e0da823a45601e35ffab
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2025-09-22 12:30:36 +0300

    initil 1

---
+++ ChapterFirst/AIntrodaction/AIntrodactionDz/introdaction_second.java
@@ -0,0 +1,23 @@
+package a_introdaction_dz;
+/*
+ * Java Developer Kit (JDK) подаёт коммантду компелятору на запуск приложения introdaction_first
+ * Компелятор преобразует спецификацию кода Java на байт-код
+ * Java Runtime Environment карелирует свои действия Java Virtual Machine(JVM), далее все эти 3 пунка обозначаются как "text"
+ * JVM получат команду на обработку класса introdaction_first
+ */
+class introdaction_first{
+
+    /*
+     *  text
+     *  JVM получат команду на обработку метода main класса introdaction_first
+     */
+    public static void main(String[] args)
+    {
+        /*
+        * text
+        * JVM получает команду на обработку Sout.out.println
+        * далее процесс вывода на экран смотри в документаци к out от Java
+        */
+        System.out.println("Hi, world");
+    }
+}
