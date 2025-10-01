c1afd85232c2bbd361fb09611e07b9e9c6fea200
TTG-ux <vasya.petrovich.63@inbox.ru>
2025-10-02 01:20:33 +0300

    123

--- Test/test.java
+++ Test/test.java
@@ -1,4 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.UncheckedIOException;
+
 public class test
 {
-
+    public static void main(String[] args)
+    {
+        boolean result = switch (ternaryBool) {
+        case TRUE -> true;
+        case FALSE -> false;
+        case FILE_NOT_FOUND -> throw new UncheckedIOException(
+            "This is ridiculous!",
+            new FileNotFoundException());
+        default -> throw new IllegalArgumentException("Seriously?!");
+        };
+    }
 }
