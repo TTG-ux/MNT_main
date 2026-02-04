5655965782443d26f3f07e16232a19690a3676d0
TTG-ux <vasya.petrovich.63@inbox.ru>
2026-02-04 15:28:27 +0300

    1

--- Collections/Task1/Main.java
+++ Collections/Task1/Main.java
@@ -8,10 +8,11 @@ public class Main {
         list.add(30);
         
         System.out.println(list); // Выведе: [10, 20, 30]
-        System.out.println("Размер списка до изменения: " + list.size()); // 3
+        System.out.println("Размер списка до изменения: " + 
+                                                list.size()); // Размер первоначального списка 3
         
         list.remove(1);
-        System.out.println("Размер списка после изменения: " + list); // Выведе: [10, 30]
+        System.out.println("Размер списка после изменения: " + list); // Выведет: [10, 30]
         
         System.out.println("В списке есть элемент 30: " + list.contains(30)); // true
         System.out.println("В списке есть элемент 50: " + list.contains(50)); // false
